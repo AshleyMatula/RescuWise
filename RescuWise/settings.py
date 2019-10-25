fad79326cd689bd01c701c8e39d9f35979dd27d2
AshleyMatula <35378128+AshleyMatula@users.noreply.github.com>
2019-10-24 20:10:03 -0400

    update settings.py

--- RescuWise/settings.py
+++ RescuWise/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ja391197!tv&r$qbptd10(ecx0visy&ur#*)se&+*j(+v9#fbp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
