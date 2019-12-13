fa3c3fe941ac0d8167f9600eedddf92556e91342
kfwojton <kfwojton@gmail.com>
2019-12-12 20:19:21 -0500

    logins

--- RescuWise/settings.py
+++ RescuWise/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # fix to convert env variable to boolean value
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'rescuwise.herokuapp.com',
